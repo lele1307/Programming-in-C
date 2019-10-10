9d089496ae789dfb28ae189a7682fc7abbb15864
lele1307 <dkh1996@126.com>
2019-10-10 12:11:21 +0100

    finshed 3.4

--- week3/3_4.c
+++ week3/3_4.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 void randomFirstlist(int *a);
 void randomNextlist(int *b,int *a);
 int main(void)
 {
     int a[33],b[33];
-    int i;
+    int i,t,r;
+    printf("Please input 1 if you want begin with randomised first line \nor input 2 with a single on in the central location ");
+    scanf("%d",&t);
+    
+        switch (t)
+        {
+        case 1:
+            for ( i = 0; i < 33; i++)
+            {
+                a[i] = (i+1)%2;
+            }
+            randomFirstlist(a);
 
-    for ( i = 0; i < 33; i++)
-    {
-        a[i] = (i+1)%2;
-    }
-    randomFirstlist(a);
-    printf("The first arrary is: \n");
-    for ( i = 0; i < 33; i++)
-    {
-        printf("%d ",a[i]);
-    }
-    printf("\nthis is next array: \n");
-    randomNextlist(b,a);
+            printf("The first arrary is: \n");
+            for ( i = 0; i < 33; i++)
+            {
+                printf("%d ",a[i]);
+            }
+            printf("\nthis is next array: \n");
+            randomNextlist(b,a);
+
+            break;
+
+        case 2:
+            srand((unsigned)(time(NULL)));
+            r=rand()%33;
+            printf("%d",r);
+            for ( i = 0; i < 33; i++)
+            {
+                a[i] = 0;
+                a[r] = 1;
+            }
+            printf("The first arrary is: \n");
+            for ( i = 0; i < 33; i++)
+            {
+                printf("%d ",a[i]);
+            }
+            printf("\nthis is next array: \n");
+            randomNextlist(b,a);
+            break;
+        
+        default:
+            printf("YOU INPUT WRONG CHARACTER!!");
+            break;   
+        }
     return 0;
 
 }
@@ -37,7 +69,7 @@ void randomFirstlist(int *a){
 void randomNextlist(int *b,int *a){
     int x,y,z;
     b[0]=0;
-    b[33]=0;
+    b[32]=0;
     for ( x = 1; x < 32; x++)
     {
         if (a[x-1]==1 && a[x]==1 && a[x+1]==1)
@@ -57,6 +89,8 @@ void randomNextlist(int *b,int *a){
         }
         
     }
-            printf("%d ",b[x]);
-
+    for ( x = 0; x < 33; x++)
+    {
+        printf("%d ",b[x]);
+    }
 }
